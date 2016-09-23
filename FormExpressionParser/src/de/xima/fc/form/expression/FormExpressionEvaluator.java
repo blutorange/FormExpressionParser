9d36dba916c264abd3ccd852215a82ea60a4e42b
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2016-09-23 16:58:35 +0200

    todo

--- FormExpressionParser/src/de/xima/fc/form/expression/FormExpressionEvaluator.java
+++ FormExpressionParser/src/de/xima/fc/form/expression/FormExpressionEvaluator.java
@@ -3,7 +3,7 @@ package de.xima.fc.form.expression;
 // TODO List
 // - nesting, for, while, scope
 // - +=, -=, = etc. ( a += b ==> a = a+b)
-// - throw §EXCEPTION('kl'), break, continue
+// - §LOG_INFO('asd',...), $LOG_ERROR, $LOG_DEBUG, $LOG_TRACE, $LOG_WARN
 // - LookUpBinding (look up in parent when not found)
 // - Functions as LangObjects, print('me') is evaluated as print -> function(){} -> functionCall
 // - unparse, variableScopeChecker
