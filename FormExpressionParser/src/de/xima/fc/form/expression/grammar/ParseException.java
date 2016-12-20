304f60956867f4844f2ff437a7d3a27192a45771
Andre Wachsmuth <wachsmuth.andre@gmx.de>
2016-12-20 13:59:20 +0100

    missing files and update gitignore

---
+++ FormExpressionParser/src/de/xima/fc/form/expression/grammar/ParseException.java
@@ -0,0 +1,246 @@
+package de.xima.fc.form.expression.grammar;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import de.xima.fc.form.expression.iface.IPositionedError;
+import de.xima.fc.form.expression.util.NullUtil;
+
+/**
+ * This exception is thrown when parse errors are encountered. You can
+ * explicitly create objects of this exception type by calling the method
+ * generateParseException in the generated parser.
+ *
+ * You can modify this class to customize your error reporting mechanisms so
+ * long as you retain the public fields.
+ */
+@ParametersAreNonnullByDefault
+public class ParseException extends Exception implements IPositionedError {
+
+	/**
+	 * The version identifier for this Serializable class. Increment only if the
+	 * <i>serialized</i> form of the class changes.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * This constructor is used by the method "generateParseException" in the
+	 * generated parser. Calling this constructor generates a new object of this
+	 * type with the fields "currentToken", "expectedTokenSequences", and
+	 * "tokenImage" set.
+	 */
+	public ParseException(final Token currentTokenVal, final int[][] expectedTokenSequencesVal,
+			final String[] tokenImageVal) {
+		super(initialise(currentTokenVal, expectedTokenSequencesVal, tokenImageVal));
+		currentToken = currentTokenVal;
+		expectedTokenSequences = expectedTokenSequencesVal;
+		tokenImage = tokenImageVal;
+	}
+
+	/**
+	 * The following constructors are for use by you for whatever purpose you
+	 * can think of. Constructing the exception in this manner makes the
+	 * exception behave in the normal way - i.e., as documented in the class
+	 * "Throwable". The fields "errorToken", "expectedTokenSequences", and
+	 * "tokenImage" do not contain relevant information. The JavaCC generated
+	 * code does not use these constructors.
+	 */
+
+	public ParseException() {
+		super();
+		this.currentToken = null;
+		this.expectedTokenSequences = null;
+		this.tokenImage = null;
+	}
+
+	/** Constructor with message. */
+	public ParseException(final String message) {
+		this(message, null);
+	}
+
+	/** Constructor with message and token. */
+	public ParseException(final String message, @Nullable final Token currentToken) {
+		super(message);
+		this.currentToken = currentToken;
+		this.expectedTokenSequences = null;
+		this.tokenImage = null;
+	}
+
+	/**
+	 * This is the last token that has been consumed successfully. If this
+	 * object has been created due to a parse error, the token followng this
+	 * token will (therefore) be the first error token.
+	 */
+	@Nullable
+	public final Token currentToken;
+
+	/**
+	 * Each entry in this array is an array of integers. Each array of integers
+	 * represents a sequence of tokens (by their ordinal values) that is
+	 * expected at this point of the parse.
+	 */
+	@Nullable
+	public final int[][] expectedTokenSequences;
+
+	/**
+	 * This is a reference to the "tokenImage" array of the generated parser
+	 * within which the parse error occurred. This array is defined in the
+	 * generated ...Constants interface.
+	 */
+	@Nullable
+	public final String[] tokenImage;
+
+	/**
+	 * It uses "currentToken" and "expectedTokenSequences" to generate a parse
+	 * error message and returns it. If this object has been created due to a
+	 * parse error, and you do not catch it (it gets thrown from the parser) the
+	 * correct error message gets displayed.
+	 */
+	private static String initialise(final Token currentToken, final int[][] expectedTokenSequences,
+			final String[] tokenImage) {
+		final String eol = System.getProperty("line.separator", "\n");
+		final StringBuffer expected = new StringBuffer();
+		int maxSize = 0;
+
+		final Set<String> expectedSet = new TreeSet<>();
+		for (int i = 0; i < expectedTokenSequences.length; i++) {
+			if (maxSize < expectedTokenSequences[i].length) {
+				maxSize = expectedTokenSequences[i].length;
+			}
+			for (int j = 0; j < expectedTokenSequences[i].length; j++) {
+				expected.append(tokenImage[expectedTokenSequences[i][j]]).append(' ');
+			}
+			if (expectedTokenSequences[i][expectedTokenSequences[i].length - 1] != 0) {
+				expected.append("...");
+			}
+			expectedSet.add(expected.toString());
+			expected.setLength(0);
+		}
+		for (final String s : expectedSet)
+			expected.append(s).append(eol).append("    ");
+
+		String retval = "Encountered ";
+		Token tok = currentToken.next;
+		for (int i = 0; i < maxSize; i++) {
+			if (i != 0)
+				retval += ", ";
+			if (tok.kind == 0) {
+				retval += tokenImage[0];
+				break;
+			}
+			retval += tokenImage[tok.kind];
+			retval += " \"";
+			retval += add_escapes(tok.image);
+			tok = tok.next;
+		}
+		retval += "\" at line " + currentToken.next.beginLine + ", column " + currentToken.next.beginColumn;
+		retval += "." + eol;
+		if (expectedTokenSequences.length == 1) {
+			retval += "Was expecting:" + eol + "    ";
+		}
+		else {
+			retval += "Was expecting one of:" + eol + "    ";
+		}
+		retval += expected.toString();
+		return retval;
+	}
+
+	/**
+	 * The end of line string for this machine.
+	 */
+	protected final String eol = NullUtil.or(System.getProperty("line.separator", "\n"), "\n");
+
+	/**
+	 * Used to convert raw characters to their escaped version when these raw
+	 * version cannot be used as part of an ASCII string literal.
+	 */
+	static String add_escapes(@Nullable String str) {
+		str = NullUtil.or(str, "");
+		final StringBuffer retval = new StringBuffer();
+		char ch;
+		for (int i = 0; i < str.length(); i++) {
+			switch (str.charAt(i)) {
+			case 0:
+				continue;
+			case '\b':
+				retval.append("\\b");
+				continue;
+			case '\t':
+				retval.append("\\t");
+				continue;
+			case '\n':
+				retval.append("\\n");
+				continue;
+			case '\f':
+				retval.append("\\f");
+				continue;
+			case '\r':
+				retval.append("\\r");
+				continue;
+			case '\"':
+				retval.append("\\\"");
+				continue;
+			case '\'':
+				retval.append("\\\'");
+				continue;
+			case '\\':
+				retval.append("\\\\");
+				continue;
+			default:
+				if ((ch = str.charAt(i)) < 0x20 || ch > 0x7e) {
+					final String s = "0000" + Integer.toString(ch, 16);
+					retval.append("\\u" + s.substring(s.length() - 4, s.length()));
+				}
+				else {
+					retval.append(ch);
+				}
+				continue;
+			}
+		}
+		return NullUtil.checkNotNull(retval.toString());
+	}
+
+	@Override
+	public boolean isPositionInformationAvailable() {
+		return currentToken != null;
+	}
+
+	@Override
+	public int getBeginLine() {
+		final Token currentToken = this.currentToken;
+		if (currentToken == null)
+			return 0;
+		final Token nextToken = currentToken.next;
+		return nextToken != null ? nextToken.beginLine : currentToken.beginLine;
+	}
+
+	@Override
+	public int getEndLine() {
+		final Token currentToken = this.currentToken;
+		if (currentToken == null)
+			return 0;
+		final Token nextToken = currentToken.next;
+		return nextToken != null ? nextToken.endLine : currentToken.endLine;
+	}
+
+	@Override
+	public int getBeginColumn() {
+		final Token currentToken = this.currentToken;
+		if (currentToken == null)
+			return 0;
+		final Token nextToken = currentToken.next;
+		return nextToken != null ? nextToken.beginColumn : currentToken.beginColumn;
+	}
+
+	@Override
+	public int getEndColumn() {
+		final Token currentToken = this.currentToken;
+		if (currentToken == null)
+			return 0;
+		final Token nextToken = currentToken.next;
+		return nextToken != null ? nextToken.endColumn : currentToken.endColumn;
+	}
+}
---
+++ FormExpressionParser/src/de/xima/fc/form/expression/grammar/TokenMgrError.java
@@ -0,0 +1,178 @@
+package de.xima.fc.form.expression.grammar;
+
+import de.xima.fc.form.expression.iface.IPositionedError;
+import de.xima.fc.form.expression.util.CmnCnst;
+import de.xima.fc.form.expression.util.NullUtil;
+
+/** Token Manager Error. */
+public class TokenMgrError extends Error implements IPositionedError {
+	/**
+	 * The version identifier for this Serializable class. Increment only if the
+	 * <i>serialized</i> form of the class changes.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/*
+	 * Ordinals for various reasons why an Error of this type can be thrown.
+	 */
+
+	/**
+	 * Lexical error occurred.
+	 */
+	static final int LEXICAL_ERROR = 0;
+
+	/**
+	 * An attempt was made to create a second instance of a static token
+	 * manager.
+	 */
+	static final int STATIC_LEXER_ERROR = 1;
+
+	/**
+	 * Tried to change to an invalid lexical state.
+	 */
+	static final int INVALID_LEXICAL_STATE = 2;
+
+	/**
+	 * Detected (and bailed out of) an infinite loop in the token manager.
+	 */
+	static final int LOOP_DETECTED = 3;
+
+	/**
+	 * Indicates the reason why the exception is thrown. It will have one of the
+	 * above 4 values.
+	 */
+	int errorCode;
+
+	private int beginLine, beginColumn, endLine, endColumn;
+	private boolean hasPosition;
+
+	/**
+	 * Replaces unprintable characters by their escaped (or unicode escaped)
+	 * equivalents in the given string
+	 */
+	protected static final String addEscapes(final String str) {
+		final StringBuffer retval = new StringBuffer();
+		char ch;
+		for (int i = 0; i < str.length(); i++) {
+			switch (str.charAt(i)) {
+			case 0:
+				continue;
+			case '\b':
+				retval.append("\\b");
+				continue;
+			case '\t':
+				retval.append("\\t");
+				continue;
+			case '\n':
+				retval.append("\\n");
+				continue;
+			case '\f':
+				retval.append("\\f");
+				continue;
+			case '\r':
+				retval.append("\\r");
+				continue;
+			case '\"':
+				retval.append("\\\"");
+				continue;
+			case '\'':
+				retval.append("\\\'");
+				continue;
+			case '\\':
+				retval.append("\\\\");
+				continue;
+			default:
+				if ((ch = str.charAt(i)) < 0x20 || ch > 0x7e) {
+					final String s = "0000" + Integer.toString(ch, 16);
+					retval.append("\\u" + s.substring(s.length() - 4, s.length()));
+				}
+				else {
+					retval.append(ch);
+				}
+				continue;
+			}
+		}
+		return retval.toString();
+	}
+
+	/**
+	 * Returns a detailed message for the Error when it is thrown by the token
+	 * manager to indicate a lexical error.
+	 * You can customize the lexical error message by modifying
+	 * this method.
+	 * @param EOFSeen Indicates if EOF caused the lexical error curLexState
+	 * @param lexState In which state this error occurred.
+	 * @param errorLine The line number when the error occurred.
+	 * @param errorColumn The column number when the error occurred.
+	 * @param errorAfter Prefix that was seen before this error occurred.
+	 * @param curchar The offending character.
+	 * @return The customized error message.
+	 */
+	protected static String LexicalError(final boolean EOFSeen, final int lexState, final int errorLine,
+			final int errorColumn, final String errorAfter, final char curChar) {
+		final String encountered = EOFSeen ? "<EOF>" : ("\"" + String.valueOf(curChar)+ "\"");
+		return NullUtil.messageFormat(CmnCnst.Error.TOKEN_MGR_ERROR, errorLine, errorColumn, encountered, errorAfter);
+	}
+
+	/**
+	 * You can also modify the body of this method to customize your error
+	 * messages. For example, cases like LOOP_DETECTED and INVALID_LEXICAL_STATE
+	 * are not of end-users concern, so you can return something like :
+	 *
+	 * "Internal Error : Please file a bug report .... "
+	 *
+	 * from this method for such cases in the release version of your parser.
+	 */
+	@Override
+	public String getMessage() {
+		return super.getMessage();
+	}
+
+	/*
+	 * Constructors of various flavors follow.
+	 */
+
+	/** No arg constructor. */
+	public TokenMgrError() {
+	}
+
+	/** Constructor with message and reason. */
+	public TokenMgrError(final String message, final int reason) {
+		super(message);
+		errorCode = reason;
+	}
+
+	/** Full Constructor. */
+	public TokenMgrError(final boolean EOFSeen, final int lexState, final int errorLine, final int errorColumn,
+			final String errorAfter, final char curChar, final int reason) {
+		this(LexicalError(EOFSeen, lexState, errorLine, errorColumn, errorAfter, curChar), reason);
+		this.beginLine = this.endLine = errorLine;
+		this.beginColumn = this.endColumn = errorColumn;
+		this.hasPosition = errorLine > 0 && errorColumn >= 0;
+	}
+
+	@Override
+	public boolean isPositionInformationAvailable() {
+		return hasPosition;
+	}
+
+	@Override
+	public int getBeginLine() {
+		return beginLine;
+	}
+
+	@Override
+	public int getEndLine() {
+		return endLine;
+	}
+
+	@Override
+	public int getBeginColumn() {
+		return beginColumn;
+	}
+
+	@Override
+	public int getEndColumn() {
+		return endColumn;
+	}
+}
